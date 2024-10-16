03b9e40d262bc291f8a660a4dbb67e6de349f4f3
ta-matsu0407 <uu.pyonpyon@gmail.com>
2024-10-16 23:50:54 +0900

    修正_02

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+
+
+class TodoListController extends Controller
+{
+    public function index()
+    {
+        $values = Test::all();
+
+        //dd($values);
+        //phpのdie + var_dump のようなもの(dieで処理を止めて、var_dumpで内容を確認)
+
+        return view('todo_list.index', compact('values'));
+        //resources/views/todoLists/todoList.blade.php
+        //compact関数で変数に入れたものをView側に渡す(変数ではなく、文字列で渡す)
+    }
+
+}
